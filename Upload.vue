3e1a07d3451ac8be60bdbee3f8df1a263bf26463
Jack Halloween <78194804+jackhallloween21@users.noreply.github.com>
2024-04-01 22:52:14 +0530

    Create Upload.vue

---
+++ Upload.vue
@@ -0,0 +1,12 @@
+In `Upload.vue`:
+```html
+<template>
+<div id="app">
+<h1>Image Uploader</h1>
+<form @submit.prevent="handleSubmit">
+<label for="file">Choose a file:</label>
+<input type="file" id="file" name="file" ref="fileInput" v-on:change="handleFileSelect">
+<button type="submit">Upload</button>
+</form>
+<div v-if="imageUrl" class="image-container"><img :src="imageUrl" alt="Uploaded Image"></div>
+</div>
